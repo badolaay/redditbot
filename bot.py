31bcb484b611b2aad5bc3e1da12f261cf2c029ab
badolaay <(none)>
2017-04-09 08:57:33 -0400

    Added mail

--- bot.py
+++ bot.py
@@ -1,31 +1,58 @@
 import praw
+import requests
+import configparser
 
-reddit = praw.Reddit('bot1')
+config = configparser.RawConfigParser()
+config.read('config.ini')
 
-subreddit = reddit.subreddit("programming")
 
-
-def printComments():
-    print("Heres some posts from " + subreddit.display_name)
+def printComments(subreddit):
+    text = "Heres some posts from <a href=""reddit.com/r/" + subreddit.display_name + """>""" + subreddit.display_name
+    text += "</a><br>"
     i = 0
-    for submission in subreddit.hot(limit=5):
-        if (not submission.stickied and i < 1):
-            i += 1
-            print("Title: ", submission.title)
-            if (submission.selftext != ''):
-                print("Text: ", submission.selftext)
-            print("Score: ", submission.score)
-            i = 0
-            comments = submission.comments._comments
-            while (i < 1 and len(comments) > i):
-                print(comments[i].body)
+    try:
+        for submission in subreddit.hot(limit=5):
+            if (not submission.stickied and i < 5):
                 i += 1
+                text += "<strong>Title:</strong> " + submission.title + "<br>"
+                if (submission.selftext != ''):
+                    text += "<strong>Text:</strong> " + submission.selftext + "<br>"
+                text += "<strong>Score:</strong> " + submission.score + "<br>"
+                i = 0
+                comments = submission.comments._comments
+                while (i < 1 and len(comments) > i):
+                    text += comments[i].body + "<br>"
+                    i += 1
 
-            print("---------------------------------\n")
+                text += "---------------------------------<br>"
+    except:
+        pass
+    return text
 
 
-printComments()
-subreddit = reddit.subreddit("random")
-subreddit = reddit.subreddit("random")
-subreddit = reddit.subreddit("random")
-printComments()
+def send_simple_message(to, text):
+    return requests.post(
+        config.get('mail', 'url'),
+        auth=("api", config.get('mail', 'key')),
+        data={"from": config.get('mail', 'from'),
+              "to": to,
+              "subject": "Hello",
+              "html": text})
+
+
+reddit = praw.Reddit('testbot')
+
+messages = reddit.inbox.all()
+for message in messages:
+    m = message.body.split(";")
+    if (len(m) > 1):
+        email = m[0]
+        text = ''
+        for i in range(1, len(m)):
+            subreddit = reddit.subreddit(m[i])
+            text += printComments(subreddit)
+        send_simple_message(email, text)
+
+# subreddit = reddit.subreddit("random")
+# subreddit = reddit.subreddit("random")
+# subreddit = reddit.subreddit("random")
