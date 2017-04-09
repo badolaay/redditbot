ea68d00476efc14ca6f8a53e5002c685a159e8b1
badolaay <(none)>
2017-04-09 09:22:21 -0400

    Fix mail issue

--- bot.py
+++ bot.py
@@ -8,21 +8,22 @@ config.read('config.ini')
 
 def printComments(subreddit):
     text = "Heres some posts from <a href=""reddit.com/r/" + subreddit.display_name + """>""" + subreddit.display_name
-    text += "</a><br>"
+    text += "</a><br><br>"
     i = 0
     try:
-        for submission in subreddit.hot(limit=5):
+        for submission in subreddit.hot(limit=3):
             if (not submission.stickied and i < 5):
                 i += 1
                 text += "<strong>Title:</strong> " + submission.title + "<br>"
                 if (submission.selftext != ''):
                     text += "<strong>Text:</strong> " + submission.selftext + "<br>"
-                text += "<strong>Score:</strong> " + submission.score + "<br>"
-                i = 0
+                text += "<strong>Score:</strong> " + str(submission.score) + "<br>"
+                j = 0
+                text += "<strong>Comments:</strong><br>"
                 comments = submission.comments._comments
-                while (i < 1 and len(comments) > i):
-                    text += comments[i].body + "<br>"
-                    i += 1
+                while (j < 5 and len(comments) > j):
+                    text += comments[j].body + "<br><br>"
+                    j += 1
 
                 text += "---------------------------------<br>"
     except:
@@ -51,6 +52,8 @@ for message in messages:
         for i in range(1, len(m)):
             subreddit = reddit.subreddit(m[i])
             text += printComments(subreddit)
+        text += "<br>Explore some random subreddits:<br>"
+        text += printComments(reddit.subreddit("random"))
         send_simple_message(email, text)
 
 # subreddit = reddit.subreddit("random")
